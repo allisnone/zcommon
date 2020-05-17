5ad3eed5326f70c63721350817d00dc40d180877
allisnone <zgx20022002@163.com>
2020-05-17 23:04:55 +0800

    update

--- krandom.py
+++ krandom.py
@@ -21,6 +21,7 @@ def limit_price(price,rate=0.10):
     """
     return round_up(price*(1.0 + rate),2),round_up(price*(1.0-rate),2)
 
+
 class Randomkdata:
     def __init__(self,price,trand=0,rate=0.1):
         self.open = 1
